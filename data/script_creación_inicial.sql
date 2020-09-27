7a5963cbc129265a673e2c9363ea1b52b382dfb1
Maximiliano Bunirigo <maxibunirigo@gmail.com>
2020-09-27 13:01:03 -0700

    Creo la carpeta inicial con los scrips base

---
+++ data/script_creación_inicial.sql
@@ -0,0 +1,8 @@
+USE [GD2C2020]
+GO
+
+/****** Object:  Schema [NO_HAY_BACKUP]    Script Date: 9/27/2020 12:26:02 PM ******/
+CREATE SCHEMA [NO_HAY_BACKUP]
+GO
+
+
---
+++ data/“script_creación_BI.sql
@@ -0,0 +1,8 @@
+USE [GD2C2020]
+GO
+
+/****** Object:  Schema [gd_esquema]    Script Date: 9/27/2020 12:58:18 PM ******/
+CREATE SCHEMA [gd_esquema]
+GO
+
+
